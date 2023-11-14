c33d04e2d860d39871a4b940c09fe83bfd2c6080
Thomas <friar-festus@gmail.com>
2023-11-14 10:59:47 -0800

    Completed assignment 1.

---
+++ finder-app/finder.sh
@@ -0,0 +1,24 @@
+#/usr/bin/bash
+
+# First we check for the correct number of arguments,
+# exiting if there are more or less than two.
+if [ $# -ne 2 ]; then
+  echo "There must be exactly two arguments."
+  echo "Usage: $0 <directory> <searchstr>"
+  exit 1
+fi
+
+# Check that the first argument is a directory.
+if [ ! -d "$1" ]; then
+  echo "First argument must be a directory."
+  echo "Usage: $0 <directory> <searchstr>"
+  exit 1
+fi
+
+# Get the number of files in the given directory.
+num_files=$(find "$1" -type f | wc -l)
+
+# Get the number of matching lines in files.
+num_lines=$(grep "$2" "$1"/* | wc -l)
+echo "The number of files are $num_files and the number of matching lines are $num_lines."
+  
---
+++ finder-app/writer.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+# First we check for the correct number of arguments,
+# exiting if there are more or less than two.
+if [ $# -ne 2 ]; then
+  echo "There must be exactly two arguments."
+  echo "Usage: $0 <writefile> <writestr>"
+  exit 1
+fi
+
+# Create file, overwriting if it exists, and path if necessary
+# Split out directory and filename from writefile
+file="`basename "$1"`"
+dir="`dirname "$1"`"
+
+mkdir -p $dir
+echo $2 > $1
