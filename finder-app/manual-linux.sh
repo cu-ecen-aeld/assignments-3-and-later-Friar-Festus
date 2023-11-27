dc463d6102cdd92c51eb84fdd3da5a1530b587b6
Thomas <friar-festus@gmail.com>
2023-11-26 23:14:32 -0800

    Add SYSROOT variable for lib & lib64 paths.

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -12,7 +12,7 @@ BUSYBOX_VERSION=1_33_1
 FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
 CROSS_COMPILE=aarch64-none-linux-gnu-
-SYSROOT=${CROSS_COMPILE}gcc -print-sysroot
+SYSROOT=$(${CROSS_COMPILE}gcc -print-sysroot)
 
 if [ $# -lt 1 ]
 then
